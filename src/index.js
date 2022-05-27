7918387dcbc563acdc1a9baf9c0e133a4c1f2f32
TimiMaliki <malikitimi@gmail.com>
2022-05-27 14:50:44 -0700

    .length commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
- let a = [4,12,3,4,4,5];
+/* let a = [4,12,3,4,4,5];
 let b = ['Timi','emma','orji','ben','phlex','jp'];
 
 
 console.log(a[3]);
 a[3] = 9;
  console.log(a);
+ */
 
 // so i changed the third array from 4 to 9 
 // making some progress tho
 
 
 //data type
-console.log(typeof a);
+// console.log(typeof a);
+
+let a = [4,12,3,4,4,5];
+
+a[20] = 75;
+
+console.log(a.length);
+
