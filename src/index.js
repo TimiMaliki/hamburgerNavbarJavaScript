6ec5d726493a29b52d108c8a148842366b3eab00
TimiMaliki <malikitimi@gmail.com>
2022-05-27 14:40:32 -0700

    typeof commit

--- src/index.js
+++ src/index.js
@@ -3,3 +3,12 @@ let b = ['Timi','emma','orji','ben','phlex','jp'];
 
 
 console.log(a[3]);
+a[3] = 9;
+ console.log(a);
+
+// so i changed the third array from 4 to 9 
+// making some progress tho
+
+
+//data type
+console.log(typeof a);
